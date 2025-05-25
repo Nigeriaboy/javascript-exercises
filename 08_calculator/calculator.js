f3ee49eb2ffa601eba1ec1c69e5e35dee9779198
Nigeriaboy <abdurahmonabdullah247@gmail.com>
2025-05-25 22:24:08 +0100

    solved the calculator and palindromes problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((accumulator, num) => accumulator + num, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args[0] - args[1];
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, num) => accumulator + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {	
+  return arr.reduce((accumulator, num) => accumulator * num, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let powered = num; // save the value of number to be powered
+  for (let i = 1; i < power; i++){
+    powered = powered * num;
+  }
+  return powered;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+
+  for (let i = 1; i < num; i++){
+    factorial = factorial * (i + 1);
+  }
+  return factorial;
 };
 
 // Do not edit below this line
