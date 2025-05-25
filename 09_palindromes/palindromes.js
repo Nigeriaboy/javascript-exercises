a7852d151b8bfb57cb8c82cf78d1730bad28a825
Nigeriaboy <abdurahmonabdullah247@gmail.com>
2025-05-25 22:21:16 +0100

    solved the calculator and palindromes problems

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let upperStr = str.toUpperCase();
+    let arrayStr = upperStr.split(''); // Convert it into an array of characters
 
+    function storeAlphaNumeric (arrayStr){
+        let arr = [];
+
+        for (let char of arrayStr){
+            if (/[A-Z0-9]/.test(char)){ // using regex to check if the char is an alphabet or a number
+                arr.push(char);
+            } 
+        }
+        return arr;
+    }
+
+    let alphaNumeric = storeAlphaNumeric(arrayStr); // store only alphanumeric characters in an array
+    let originalAlphaNumeric = alphaNumeric.join(''); // convert the array back to a string
+    let reverseAlphaNumeric = alphaNumeric.reverse().join(''); // reverse the array and convert it back to a string
+
+    return originalAlphaNumeric === reverseAlphaNumeric; // Return true if it is a palindrome, false otherwise
 };
 
 // Do not edit below this line
